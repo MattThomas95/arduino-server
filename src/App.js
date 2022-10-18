7da4910e5614e66096663fe4bd461b9caef8d36f
MattThomas95 <matt.thomas95@gmail.com>
2022-10-17 20:59:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [response, setResponse] = useState("");
+  const [oldId, setOldId] = useState("");
+  const [recentAmount, setRecent] = useState("");
+  const [action, setAction] = useState("");
+  const options = { method: "GET", headers: { accept: "application/json" } };
+
+  const [message, setMessage] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+    // 3p9XMiyCYlkZQT28dUGjE1EjfpogTKediFp2xPxc
+    console.log("value is:", event.target.value);
+  };
+
+  // todo add input field for access_token
+  useEffect(() => {
+    if(message){
+      const interval = setInterval(() => {
+        console.log("This will run every 10 seconds!");
+        fetch(`https://shielded-bastion-43828.herokuapp.com/https://streamlabs.com/api/v1.0/donations?access_token=${message}`, options)
+          .then((response) => response.json())
+          .then((response) => {
+            setResponse(response);
+            console.log("response", response);
+          })
+          .catch((err) => console.error(err));
+      }, 10000);
+      return () => clearInterval(interval);
+
+    }
+  }, [message]);
+
+  useEffect(() => {
+    if (response) {
+      const amount = response["data"][0]["amount"];
+      const donationId = response["data"][0]["donation_id"];
+      console.log(donationId, "id");
+      if (donationId !== oldId) {
+        console.log(response["data"][0]);
+
+        if (amount >= 15) {
+          console.log("SHOOT");
+          setAction("SHOOT");
+          // Run esp code here
+        } else {
+          console.log("DONT SHOOT");
+          setAction("DONT SHOOT");
+        }
+
+        setRecent(amount);
+      }
+      setOldId(donationId);
+      console.log(oldId, "olde");
+    }
+  }, [response]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-screen h-screen p-10">
+        <div className="flex justify-content-center">
+        <h2 className="flex justify-content-center">access_token:</h2>
+        <input
+        className="border mx-3"
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+          value={message}
+        />
+
+        </div>
+        <div>
+          Last Donation Amount {recentAmount} {action}
+        </div>
+        <ul>{/* {response["data"]} */}</ul>
+      </div>
+    </>
   );
 }
 
